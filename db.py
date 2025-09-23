8b9fdd2c97bcd3a19698bc7364c47aeaa1accda0
Jon <jonathan.martinez@maxikash.com>
2025-09-22 18:19:53 -0600

    Cambios auditorias tablas

--- db.py
+++ db.py
@@ -3,12 +3,18 @@ import os
 import mysql.connector
 from mysql.connector import Error
 
-def get_connection():
-    """Retorna una conexión a la base de datos usando variables de entorno y ajusta zona horaria."""
+def get_connection(database=None):
+    """
+    Retorna una conexión a la base de datos usando variables de entorno
+    y ajusta la zona horaria a America/Mexico_City.
+    
+    Parámetros:
+        database (str): nombre de la base de datos a usar. Si es None, usa la principal.
+    """
     db_config = {
         'user': os.environ.get('DB_USER'),
         'password': os.environ.get('DB_PASSWORD'),
-        'database': os.environ.get('DB_NAME'),
+        'database': database if database else os.environ.get('DB_NAME'),
         'unix_socket': f"/cloudsql/{os.environ.get('DB_CONNECTION_NAME')}"
     }
     try:
@@ -18,5 +24,5 @@ def get_connection():
         cursor.close()
         return conn
     except Error as e:
-        print(f"[DB ERROR] No se pudo conectar a la base de datos: {e}")
+        print(f"[DB ERROR] No se pudo conectar a la base de datos {db_config['database']}: {e}")
         return None
